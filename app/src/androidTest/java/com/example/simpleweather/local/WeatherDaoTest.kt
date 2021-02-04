f8a4f1d95d123b2fac1c6e6f59e2607647beedc3
experienzze <exper1enzzze@gmail.com>
2021-02-04 20:37:45 +0800

    try to test coroutines flow

---
+++ app/src/androidTest/java/com/example/simpleweather/local/WeatherDaoTest.kt
@@ -0,0 +1,76 @@
+package com.example.simpleweather.local
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.example.simpleweather.local.model.LocationDB
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.asExecutor
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class WeatherDaoTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: WeatherDataBase
+    private lateinit var dao: WeatherDao
+
+    private val testDispatcher = TestCoroutineDispatcher()
+    private val testScope = TestCoroutineScope(testDispatcher)
+
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            WeatherDataBase::class.java
+        )
+            .setTransactionExecutor(testDispatcher.asExecutor())
+            .setQueryExecutor(testDispatcher.asExecutor())
+            .allowMainThreadQueries()
+            .build()
+
+        dao = database.weatherDao()
+    }
+
+    @After
+    fun teardown() {
+        database.close()
+    }
+
+    @Test
+    fun saveNewLocationTest() = testScope.runBlockingTest {
+        val location = LocationDB(
+            108.343434F,
+            102.232323F,
+            "some long string with full address",
+            "Big City",
+            "county of Big City",
+            "Big State",
+            "AwesomeLand"
+        )
+
+        dao.saveNewLocation(location)
+        val result2 = async {
+            dao.getSavedLocations().take(1).toList().flatten()
+        }
+
+        assertThat(result2.await()).contains(location)
+    }
+}
