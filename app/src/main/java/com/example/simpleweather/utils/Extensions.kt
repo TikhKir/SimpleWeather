d5edc44d33978516896b4efbe03e691375daa4c9
experienzze <exper1enzzze@gmail.com>
2021-03-29 21:00:12 +0800

    takeout searchWathcerFlow to Extensions.kt

--- app/src/main/java/com/example/simpleweather/utils/Extensions.kt
+++ app/src/main/java/com/example/simpleweather/utils/Extensions.kt
@@ -1,6 +1,28 @@
 package com.example.simpleweather.utils
 
+import androidx.appcompat.widget.SearchView
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import java.text.DecimalFormat
+
+@ExperimentalCoroutinesApi
+fun SearchView.searchWatcherFlow(): Flow<String> = callbackFlow {
+    send("")
+    val listener = object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?): Boolean {
+            return false
+        }
+
+        override fun onQueryTextChange(newText: String?): Boolean {
+            offer(newText)
+            return false
+        }
+    }
+    setOnQueryTextListener(listener)
+    awaitClose()
+}
 
 fun Float.toUIFormat(): String {
     val decimalFormat = DecimalFormat("#.#")
